00b43a89fc60f1ffb1f5e5e463f084b2d9db5803
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-03-13 10:18:08 +0100

    Create student_details.py

---
+++ student_details.py
@@ -0,0 +1,6 @@
+first_name="Kayra"
+last_name="Çelikoğlu"
+index_number=35357
+nationality="Turkish"
+starting_date="29.07.2025"
+courses="Math","OOP","Polish","İntro digi tech","Telecommunications"
