822bb71929106bd67c2dbac2c6670394cdf614c0
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-03-13 10:23:35 +0100

    Create student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,15 @@
+student1 = {
+    'first_name': 'Kayra',
+    'last_name': 'Çelikoğlu',
+    'index_number': 35357,
+    'nationality': 'Turkish',
+    'starting_date': '29.07.2025',
+    'courses': ["Math", "OOP", "Polish", "İntro digi tech", "Telecommunications", "Marketing"]
+}
+
+print("First Name:", student1['first_name'])
+print("Last Name:", student1['last_name'])
+print("Index Number:", student1['index_number'])
+print("Nationality:", student1['nationality'])
+print("Starting Date:", student1['starting_date'])
+print("Courses:", ", ".join(student1['courses']))
