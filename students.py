9ce4d8f7141f4a022b2f03c8e75873b66544fc83
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-03-13 10:58:08 +0100

    Update students.py

--- students.py
+++ students.py
@@ -12,7 +12,7 @@ students = [
         'last_name': 'Bicerbay',
         'index_number': 35451,
         'nationality': 'Turkish',
-        'starting_date': '12.03.2025',
+        'starting_date': '13.03.2025',
         'courses': ["Math", "Physics", "Programming", "History"]
     },
     {
@@ -25,5 +25,18 @@ students = [
     }
 ]
 
-for student in students:
-    print(f"Name: {student['first_name']} {student['last_name']}, Index: {student['index_number']}")
+def add_student(first_name, last_name, index_number, nationality, starting_date, courses):
+    new_student = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'index_number': index_number,
+        'nationality': nationality,
+        'starting_date': starting_date,
+        'courses': courses
+    }
+    students.append(new_student)
+    print(f"Student {first_name} {last_name} is added")
+
+def display_students():
+    for student in students:
+        print(f"Name: {student['first_name']} {student['last_name']}, Index: {student['index_number']}")
