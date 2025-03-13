a324e9a817032b89822303d584af1a3bc1e78cf4
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-03-13 10:19:03 +0100

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -3,4 +3,4 @@ last_name="Çelikoğlu"
 index_number=35357
 nationality="Turkish"
 starting_date="29.07.2025"
-courses="Math","OOP","Polish","İntro digi tech","Telecommunications"
+courses="Math","OOP","Polish","İntro digi tech","Telecommunications","Marketing"
