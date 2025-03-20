2341455ce1b85414a55af10068d2442e2360dc6d
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-03-20 10:17:02 +0100

    Update animals.py

--- animals.py
+++ animals.py
@@ -2,9 +2,7 @@ animals = [
     {"name": "cat", "group": "mammal", "number_of_legs": 4, "skills": ["climbing", "jumping"]},
     {"name": "dog", "group": "mammal", "number_of_legs": 4, "skills": ["running", "swimming"]},
     {"name": "eagle", "group": "bird", "number_of_legs": 2, "skills": ["flying"]},
-    {"name": "frog", "group": "amphibian", "number_of_legs": 4, "skills": ["jumping", "swimming"]},
     {"name": "snake", "group": "reptile", "number_of_legs": 0, "skills": ["slithering", "swimming"]},
-    {"name": "shark", "group": "fish", "number_of_legs": 0, "skills": ["swimming"]},
     {"name": "butterfly", "group": "insect", "number_of_legs": 6, "skills": ["flying"]},
     {"name": "spider", "group": "arachnid", "number_of_legs": 8, "skills": ["climbing", "can do webs"]},
 ]
