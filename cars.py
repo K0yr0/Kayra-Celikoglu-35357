b8eaa9185c83a517336a0a7501c7bc6b0d145d89
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-03-27 11:03:43 +0100

    Create cars.py

---
+++ cars.py
@@ -0,0 +1,33 @@
+class Engine:
+    def __init__(self, horsepower, type):
+        self.horsepower = horsepower
+        self.type = type  # e.g., Petrol, Diesel, Electric
+
+    def get_info(self):
+        return f"{self.horsepower} HP {self.type} Engine"
+
+
+class Car:
+    def __init__(self, brand, model, year, car_type, color, engine):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.type = car_type
+        self.color = color
+        self.engine = engine
+
+    def get_info(self):
+        return f"{self.year} {self.brand} {self.model} ({self.type}) - {self.color}, {self.engine.get_info()}"
+
+
+engine1 = Engine(450, "Petrol")
+engine2 = Engine(670, "Hybrid")
+engine3 = Engine(760, "Electric")
+
+car1 = Car("Porsche", "911", 2023, "Coupe", "Silver", engine1)
+car2 = Car("Ferrari", "488", 2021, "Sports", "Red", engine2)
+car3 = Car("Ford", "Mustang", 2022, "Coupe", "Blue", engine3)
+
+print(car1.get_info())
+print(car2.get_info())
+print(car3.get_info())
