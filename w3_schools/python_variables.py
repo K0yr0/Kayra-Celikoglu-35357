78f47e2fd43d9c8b3e1b1c4a62b4a0c56484f8e1
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-04-03 11:03:08 +0200

    Create python_variables.py

---
+++ w3_schools/python_variables.py
@@ -0,0 +1,49 @@
+#example1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#example2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x) #it will print x's last value that we assigned
+
+#example3
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+#example4
+x = 5
+y = "John"
+print(type(x)) #it will give us which type is our variable
+print(type(y))
+
+#example5
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+
+#example6
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John" #all of these variable names are okay to use
+#but these are not okay to use it will cause problems
+2myvar = "John"
+my-var = "John"
+my var = "John"
+
+#example7
+myVariableName = "John" #camel case
+MyVariableName = "John" #Pascal case
+my_variable_name = "John" #snake caase
