12466b8258618ec0389fd750b5b9a28c016291dc
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-04-08 10:38:58 +0200

    Update python_variables.py

--- w3_schools/python_variables.py
+++ w3_schools/python_variables.py
@@ -115,3 +115,24 @@ myfunc()
 
 print("Python is " + x) #it will print= Python is fantastic Python is awesome
 
+#example 14
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+#To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
