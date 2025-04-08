82b6a15d491c58c4ab2de3507f0e9287cc9cd7f1
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-04-08 11:24:34 +0200

    Create my_assignments.py

---
+++ my_assignments.py
@@ -0,0 +1,28 @@
+#1-print your name on the screen
+
+
+#2-create 3 different type of variable(str,int...)
+
+
+#3-Display the type of the variable you created
+
+
+#4-Write if statement (if x is bigger than a number then display something...)
+
+
+#5-Create a list called fruits with any 3 fruit names. Then print the second fruit
+
+
+#6-Use an if statement to check if a variable x is equal to 100. If it is, print "Yes", otherwise print "No"
+
+
+#7-Make a dictionary called student with keys: "name", "age", and "grade". Assign values and print the age
+
+
+#8-Add a new item "grape" to the fruits list from question 5 and print the updated list
+
+
+#9-Update the "grade" in the student dictionary to "A+" and print the full dictionary
+
+
+#10-Create a class Person with an attribute name. Add a method greet() that prints "Hello, " + name. Create an object and call greet()
