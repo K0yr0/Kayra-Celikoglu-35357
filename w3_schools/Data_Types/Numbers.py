c95e536a41d1f51a8fee1a68742caa7067fad070
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-04-08 22:44:15 +0200

    Update Numbers.py

--- w3_schools/Data_Types/Numbers.py
+++ w3_schools/Data_Types/Numbers.py
@@ -30,6 +30,7 @@ x = memoryview(bytes(5))	memoryview
 x = None	NoneType
 """
 
+#NUMBERS
 
 #example2
 x = 1    # int
@@ -92,3 +93,21 @@ import random
 
 print(random.randrange(1, 10)) #Import the random module, and display a random number from 1 to 9:
 
+
+#CASTING
+
+#example1
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#example2
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#example3
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
