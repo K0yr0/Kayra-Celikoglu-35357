e45f7020a53332ff5375c512685776ca550a0155
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-04-09 09:07:57 +0200

    Update Strings2.py

--- w3_schools/strings/Strings2.py
+++ w3_schools/strings/Strings2.py
@@ -1,1 +1,21 @@
+#MODIFY STRINGS
 
+#example1
+a = "Hello, World!"
+print(a.upper()) #all uppercase
+
+#example2
+a = "Hello, World!"
+print(a.lower()) #all lowercase
+
+#example3
+a = " Hello, World! "
+print(a.strip()) #returns "Hello, World!" (Remove Whitespace)
+
+#example4
+a = "Hello, World!"
+print(a.replace("H", "J")) #turns Hello to Jello
+
+#example5
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
