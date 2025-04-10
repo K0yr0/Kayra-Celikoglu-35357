8a41eca1f219ce9f9ed37a1f96835446b973767e
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-04-10 11:33:09 +0200

    Update Lists.py

--- w3_schools/Lists/Lists.py
+++ w3_schools/Lists/Lists.py
@@ -1,1 +1,10 @@
+#mylist = ["apple", "banana", "cherry"]
 
+#example1
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+#List items are indexed, the first item has index [0], the second item has index [1] etc.
+
+#example2
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist) #Lists allow duplicate values
