2d054daa367ff890e851586d3285ef5d889729df
Kayra Çelikoğlu <kayraclkoglu@gmail.com>
2025-04-10 10:34:49 +0200

    Update Strings2.py

--- w3_schools/strings/Strings2.py
+++ w3_schools/strings/Strings2.py
@@ -19,3 +19,19 @@ print(a.replace("H", "J")) #turns Hello to Jello
 #example5
 a = "Hello, World!"
 print(a.split(",")) # returns ['Hello', ' World!']
+
+
+#STRING CONCANTENATION
+
+#example6
+a = "Hello"
+b = "World"
+c = a + b
+print(c) #it will print HelloWorld without space
+
+#example7
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c) #To add a space between them, add a " "
+
